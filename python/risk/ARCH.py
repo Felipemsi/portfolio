3c410fa6596ec9742c283aff08f3d60da257f5f2
Felipe Silva <112833298+Felipemsi@users.noreply.github.com>
2022-10-09 18:17:07 -0300

    Create ARCH.py

---
+++ python/risk/ARCH.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  9 12:18:34 2022
+
+@author: felipems
+"""
+
+import pandas as pd
+from arch import arch_model
+
+df = pd.read_csv('MSFT_2006-01-01_to_2018-01-01.csv')
+df.columns = ['date', 'open', 'high', 'low', 'close', 'volume', 'name']
+
+am = arch_model(df.close[0:], mean='Constant', vol='ARCH', p=2, dist='Normal')
+results_arch = am.fit(update_freq=3)
+results_arch.summary()
